7209ddb8223de90a9b9644a5d66ac448148c5922
Theo Winter <theo.winter@gmail.com>
2014-03-24 11:35:13 +0100

    added file "AdditionalClassExample.cs"

---
+++ SeeSharpWithC#/AdditionalClassExample.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace SeeSharpWithC
+{
+	public class AdditionalClassExample
+	{
+		public AdditionalClassExample ()
+		{
+		}
+
+		public void CoolMethodInOtherClass ()
+		{
+			Console.WriteLine ("Hello there!");
+		}
+	}
+}
+
