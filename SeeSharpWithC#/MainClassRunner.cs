2f35a1eef75d9adc48cb582257b7f7e9ad757e2b
Theo Winter <theo.winter@gmail.com>
2014-03-24 10:47:11 +0100

    added some comments about version control in xamarin

--- SeeSharpWithC#/MainClassRunner.cs
+++ SeeSharpWithC#/MainClassRunner.cs
@@ -1,10 +1,16 @@
 using System;
 
+/* Note to self about using git version control in xamarin:
+ * 1. it's best to use an empty repo for a new project instead of initializing it right away
+ * 2. pull is called "update solution"
+ * 3. commit is called "commit solution"
+ * */
+
 namespace SeeSharpWithC
 {
 	public class MainClassRunner
 	{
-		public static void Main()
+		public static void Main() //There can be only one main method in a project
 		{
 			System.Console.WriteLine("Hello, World!");
 		}
